f77af75cca6920ec5796643d22c876d60a4c6d90
MaarianaaPa <reyes.palaciosmariana606@gmail.com>
2023-02-20 17:29:05 -0600

    Tarea2

---
+++ Tarea2.java
@@ -0,0 +1,20 @@
+package unam.fes.poo.tarea2;
+
+import java.util.Scanner;
+
+public class Tarea2 {
+
+    public static void main(String[] args) {
+        Scanner lector = new Scanner (System.in);
+        
+        System.out.println("Ingrese nombre, profesion y pais");
+        String cadena = "";
+        String[] espacio;
+        cadena = lector.nextLine();
+        espacio = cadena.split(" ");
+        
+        System.out.println("Nombre: " + espacio[0]);
+        System.out.println("Profesion: " + espacio[0]);
+        System.out.println("Pais: " + espacio[2]);
+    }
+}
